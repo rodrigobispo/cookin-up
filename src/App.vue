074ab8e0cf4037e21b47e3d2857970e43b34c690
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-02-09 11:54:28 -0300

    Componente conteúdo principal com uso de for para loop.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
 <script lang="ts">
 import Banner from './components/Banner.vue'
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
+
 export default {
-  components: { Banner }
+  components: { Banner, ConteudoPrincipal }
 }
 </script>
---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="conteudo-principal">
+    <section>
+      <span class="substituto-lg sua-lista-texto">
+        Sua lista:
+      </span>
+
+      <ul class="ingredientes-sua-lista">
+        <li v-for="ingrediente in ingredientes" class="ingrediente">
+          {{ ingrediente }}
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      ingredientes: ['Alho', 'Manteiga', 'Leite']
+    }
+  }
+})
+</script>
