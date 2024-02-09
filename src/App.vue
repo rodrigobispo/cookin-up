86c5f45d0663ab24f0f4351711f8f14486c9ac16
rodrigobispo <rodrigoaugustocb@gmail.com>
2024-02-09 07:57:56 -0300

    Construção de primeiro componente, banner, chamado pelo app principal.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Meu projeto Vue!</h1>
+  <Banner />
 </template>
+
+<script lang="ts">
+import Banner from './components/Banner.vue'
+export default {
+  components: { Banner }
+}
+</script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="../assets/images/logo.svg" alt="Logo do Cookin' Up" class="logo">
+      <p class="cabecalho-lg frase-cabecalho">
+        <span class="texto-verde">Um banquete de ideias para</span>
+        despertar o chefe que há em você
+      </p>
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
+    </div>
+    <img src="../assets/images/foto-banner.png" alt="" class="foto-banner">
+  </header>
+</template>
